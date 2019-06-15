11a5330a4e289cbf6865c00e98d2bcf1c2726713
Tossapon Nuanchuay <ntossapo@gmail.com>
2019-06-15 12:43:48 +0700

    add more files

---
+++ SQL.R
@@ -0,0 +1,16 @@
+install.packages("sqldf")
+library(sqldf)
+
+query <- "SELECT * FROM mtcars LIMIT 10;"; sqldf(query)
+query2 <- "SELECT * FROM mtcars WHERE hp < 100 LIMIT 10;"; sqldf(query2)
+query3 <- "SELECT * FROM mtcars WHERE hp < 100 ORDER BY hp DESC LIMIT 10;"; sqldf(query3)
+query4 <- "
+SELECT 
+  cyl, 
+  AVG(hp) AS average_hp,
+  MAX(hp) AS max_hp,
+  MIN(hp) AS min_hp
+FROM
+  mtcars 
+GROUP BY cyl
+"; v <- sqldf(query4)
--- function.R
+++ function.R
@@ -11,7 +11,7 @@ myTriple <- function(x) x * 3; myTriple(v)
 
 cube <- function(x, power = 3) x ** power; cube(2); cube(2, 4); cube(2, power = 8)
 
-rollDices <- function () sum(sample(1:6, 2))
+rollDices <- function () sum(sample(1:6, 2, replace = T))
 
 print(rollDices())
 
